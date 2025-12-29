fb61d8a0e07e2648500862a484213df9a707c938
Kim Audrey Magan <work.kimmagan@gmail.com>
2025-12-30 01:37:12 +0800

    Added session but commented it out

--- server/app.py
+++ server/app.py
@@ -1,5 +1,8 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, request
+
+app = Flask(__name__)
+app.secret_key = "my_secret_key"  
 
 # Point to the root directory (one level up from /server)
 base_dir = os.path.abspath('..')
@@ -12,7 +15,43 @@ app = Flask(__name__,
 
 @app.route('/')
 def index():
+    print(session.get("fullname"))
     return render_template('index.html')
+
+
+
+# Route for when analyze with OCR 
+# @app.route('/submit', methods=["POST"])
+# def submit():
+#     print("Got here!")
+
+#     names = [
+#         "fullname",
+#         "dateofbirth",
+#         "gender",
+#         "nationality",
+#         "email",
+#         "phonenumber",
+#         "alternatephone",
+#         "address",
+#         "highesteducationlevel",
+#         "school",
+#         "course",
+#         "yeargraduated",
+#         "currentemployer",
+#         "jobtitle",
+#         "yearsofexperience",
+#         "monthlysalary",
+#         "sssnumber",
+#         "tinnumber",
+#         "philhealthnumber",
+#         "pagibignumber"
+#     ]
+
+#     for name in names:
+#         session[name] = request.form.get(name)
+
+#     return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
